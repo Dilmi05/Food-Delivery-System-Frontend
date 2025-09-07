9c7b515b8ab65565d7a9a0e2189362e0125e5aae
Dilmi Amandha <amandhadilmi@gmail.com>
2025-09-07 15:50:48 +0530

    Create FoodItem.jsx and FoodDisplay.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import StoreContextProvider from './context/StoreContext.jsx';
  
 
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
+<StoreContextProvider>
   <App />
+</StoreContextProvider>
+   
 </BrowserRouter>
      
     
