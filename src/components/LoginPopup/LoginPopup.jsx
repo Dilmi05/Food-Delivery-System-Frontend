613bbdc66706e7b906fdccc98b379166a94919c1
Dilmi Amandha <amandhadilmi@gmail.com>
2025-09-18 19:39:20 +0530

    edited Navbar.jsx and Cart.jsx

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -1,32 +1,80 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./LoginPopup.css";
 import { useState } from "react";
 import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/StoreContext";
+import axios from "axios";
 
 
 const LoginPopup = ({ setShowLogin }) => {
-    const [currrentState, setCurrentState] = useState("login")
+
+    const {url,setToken} = useContext(StoreContext)
+
+    const [currentState, setCurrentState] = useState("login")
+    const [data,setData] = useState({
+        name : "",
+        email:"",
+        password:""
+    })
+
+    const onChangeHandler = (event)=>{
+        const name = event.target.name;
+        const value = event.target.value;
+        setData(data=>({...data,[name]:value}))
+
+
+    }
+
+    const onLogin = async(event)=>{
+        event.preventDefault()
+        let newUrl = url;
+        if (currentState==="login") {
+            newUrl += "/api/user/login"
+            
+        }
+
+        else{
+            newUrl +="/api/user/register"
+        }
+
+      const response = await axios.post(newUrl,data);
+
+      if(response.data.success){
+        setToken(response.data.token);
+        localStorage.setItem("token",response.data.token)
+        setShowLogin(false)
+
+
+      }
+      else{
+        alert(response.data.message)
+      }
+
+
+    }
+
+   
     return (
         <div className="login-popup">
-            <form className="login-popup-container">
+            <form onSubmit={onLogin} className="login-popup-container">
                 <div className="login-popup-title">
-                    <h2>{currrentState}</h2>
+                    <h2>{currentState}</h2>
                     <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt=''>
                     </img>
                 </div>
                 <div className="login-popup-inputs">
-                    {currrentState === "Login" ? (
-                        <input type="text" placeholder="Your name" required />
+                    {currentState === "Login" ? (
+                        <input name="name" onChange={onChangeHandler} value={data.name} type="text" placeholder="Your name" required />
                     ) : null}
-                    <input type="email" placeholder="Your email" required />
-                    <input type="password" placeholder="Password" required />
+                    <input name="email" onChange={onChangeHandler} value={data.email} type="email" placeholder="Your email" required />
+                    <input name="password" onChange={onChangeHandler} value={data.password} type="password" placeholder="Password" required />
                 </div>
-                <button>{currrentState === "sign up" ? "Create account" : "Login"}</button>
+                <button type="submit">{currentState === "sign up" ? "Create account" : "Login"}</button>
                 <div className="login-popup-condition">
                     <input type="checkbox" required></input>
                     <p>By continuing,i agree to the terms of use & privacy policy.</p>
                 </div>
-                {currrentState === "Login"
+                {currentState === "Login"
                     ? <p>Create a new account?<span onClick={()=>setCurrentState("Sign Up")}>Click here</span></p>
                     : <p>Already have an account?<span onClick={()=>setCurrentState("Login")}>Login here</span></p>
 
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -8,7 +8,7 @@ import {useNavigate} from 'react-router-dom';
 
 const Cart = () => {
 
-  const { cartItems, food_list, removeFromCart,getTotalCartAmount } = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart,getTotalCartAmount,url } = useContext(StoreContext);
   const navigate = useNavigate();
   return (
     <div className="cart">
@@ -34,7 +34,7 @@ const Cart = () => {
             return (
               <div>
                 <div className="cart-item-title cart-item-item">
-                  <img src={item.image}></img>
+                  <img src={url+"/images/"+item.image}></img>
                   <p>{item.name}</p>
                   <p>${item.price}</p>
                   <p>{cartItems[item._id]}</p>
